6804860a91170019f64c7ed5c8e74354072070b0
Stephen Colebourne <scolebourne@joda.org>
2014-03-22 22:25:02 +0000

    Add Temporals.convertAmount()
    Converts an amount of time to a different unit
    Fixes #6

--- src/main/java/org/threeten/extra/Temporals.java
+++ src/main/java/org/threeten/extra/Temporals.java
@@ -33,9 +33,18 @@ package org.threeten.extra;
 
 import static java.time.temporal.ChronoField.DAY_OF_WEEK;
 import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.ERAS;
+import static java.time.temporal.ChronoUnit.FOREVER;
+import static java.time.temporal.ChronoUnit.WEEKS;
 
+import java.time.DateTimeException;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.IsoFields;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalUnit;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import java.util.Objects;
 
 /**
  * Additional utilities for working with temporal classes.
@@ -116,6 +125,92 @@ public final class Temporals {
                 }
             }
         },
+    }
+
+    //-------------------------------------------------------------------------
+    /**
+     * Converts an amount from one unit to another.
+     * <p>
+     * This works on the units in {@code ChronoUnit} and {@code IsoFields}.
+     * The {@code DAYS} and {@code WEEKS} units are handled as exact multiple of 24 hours.
+     * The {@code ERAS} and {@code FOREVER} units are not supported.
+     *
+     * @param amount  the input amount in terms of the {@code fromUnit}
+     * @param fromUnit  the unit to convert from, not null
+     * @param toUnit  the unit to convert to, not null
+     * @return the conversion array,
+     *  element 0 is the signed whole number,
+     *  element 1 is the signed remainder in terms of the input unit,
+     *  not null
+     * @throws DateTimeException if the units cannot be converted
+     * @throws UnsupportedTemporalTypeException if the units are not supported
+     * @throws ArithmeticException if numeric overflow occurs
+     */
+    public static long[] convertAmount(long amount, TemporalUnit fromUnit, TemporalUnit toUnit) {
+        Objects.requireNonNull(fromUnit, "fromUnit");
+        Objects.requireNonNull(toUnit, "toUnit");
+        validateUnit(fromUnit);
+        validateUnit(toUnit);
+        if (fromUnit.equals(toUnit)) {
+            return new long[] {amount, 0};
+        }
+        // precise-based
+        if (isPrecise(fromUnit) && isPrecise(toUnit)) {
+            long fromNanos = fromUnit.getDuration().toNanos();
+            long toNanos = toUnit.getDuration().toNanos();
+            if (fromNanos > toNanos ) {
+                long multiple = fromNanos / toNanos;
+                return new long[] {Math.multiplyExact(amount, multiple), 0};
+            } else {
+                long multiple = toNanos / fromNanos;
+                return new long[] {amount / multiple, amount % multiple};
+            }
+        }
+        // month-based
+        int fromMonthFactor = monthMonthFactor(fromUnit, fromUnit, toUnit);
+        int toMonthFactor = monthMonthFactor(toUnit, fromUnit, toUnit);
+        if (fromMonthFactor > toMonthFactor ) {
+            long multiple = fromMonthFactor / toMonthFactor;
+            return new long[] {Math.multiplyExact(amount, multiple), 0};
+        } else {
+            long multiple = toMonthFactor / fromMonthFactor;
+            return new long[] {amount / multiple, amount % multiple};
+        }
+    }
+
+    private static void validateUnit(TemporalUnit unit) {
+        if (unit instanceof ChronoUnit) {
+            if (unit.equals(ERAS) || unit.equals(FOREVER)) {
+                throw new UnsupportedTemporalTypeException("Unsupported TemporalUnit: " + unit);
+            }
+        } else if (unit.equals(IsoFields.QUARTER_YEARS) == false) {
+            throw new UnsupportedTemporalTypeException("Unsupported TemporalUnit: " + unit);
+        }
+    }
+
+    private static boolean isPrecise(TemporalUnit unit) {
+        return unit instanceof ChronoUnit && ((ChronoUnit) unit).compareTo(WEEKS) <= 0;
+    }
+
+    private static int monthMonthFactor(TemporalUnit unit, TemporalUnit fromUnit, TemporalUnit toUnit) {
+        if (unit instanceof ChronoUnit) {
+            switch ((ChronoUnit) unit) {
+                case MONTHS:
+                    return 1;
+                case YEARS:
+                    return 12;
+                case DECADES:
+                    return 120;
+                case CENTURIES:
+                    return 1200;
+                case MILLENNIA:
+                    return 12000;
+                default:
+                    throw new DateTimeException(
+                        String.format("Unable to convert between units: %s to %s", fromUnit, toUnit));
+            }
+        }
+        return 3;  // quarters
     }
 
 }
--- src/test/java/org/threeten/extra/TestTemporals.java
+++ src/test/java/org/threeten/extra/TestTemporals.java
@@ -37,10 +37,21 @@ import static java.time.DayOfWeek.SATURDAY;
 import static java.time.DayOfWeek.SUNDAY;
 import static java.time.Month.DECEMBER;
 import static java.time.Month.JANUARY;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertSame;
-import static org.testng.Assert.assertTrue;
+import static java.time.temporal.ChronoUnit.CENTURIES;
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.DECADES;
+import static java.time.temporal.ChronoUnit.ERAS;
+import static java.time.temporal.ChronoUnit.FOREVER;
+import static java.time.temporal.ChronoUnit.HALF_DAYS;
+import static java.time.temporal.ChronoUnit.HOURS;
+import static java.time.temporal.ChronoUnit.MILLENNIA;
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static java.time.temporal.ChronoUnit.MONTHS;
+import static java.time.temporal.ChronoUnit.NANOS;
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static java.time.temporal.ChronoUnit.WEEKS;
+import static java.time.temporal.ChronoUnit.YEARS;
+import static java.time.temporal.IsoFields.QUARTER_YEARS;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -48,11 +59,17 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalUnit;
+import java.time.temporal.UnsupportedTemporalTypeException;
 
+import org.testng.AssertJUnit;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -64,9 +81,9 @@ public class TestTemporals {
     //-----------------------------------------------------------------------
     // nextWorkingDay()
     //-----------------------------------------------------------------------
-    public void test_nextWorkingDay_serialization() throws IOException, ClassNotFoundException {
+    @Test public void test_nextWorkingDay_serialization() throws IOException, ClassNotFoundException {
         TemporalAdjuster nextWorkingDay = Temporals.nextWorkingDay();
-        assertTrue(nextWorkingDay instanceof Serializable);
+        AssertJUnit.assertTrue(nextWorkingDay instanceof Serializable);
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ObjectOutputStream oos = new ObjectOutputStream(baos);
@@ -74,64 +91,64 @@ public class TestTemporals {
         oos.close();
 
         ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()));
-        assertSame(ois.readObject(), nextWorkingDay);
+        AssertJUnit.assertSame(ois.readObject(), nextWorkingDay);
     }
 
-    public void test_nextWorkingDay() {
+    @Test public void test_nextWorkingDay() {
         for (Month month : Month.values()) {
             for (int i = 1; i <= month.length(false); i++) {
                 LocalDate date = LocalDate.of(2007, month, i);
                 LocalDate test = (LocalDate) Temporals.nextWorkingDay().adjustInto(date);
-                assertTrue(test.isAfter(date));
-                assertFalse(test.getDayOfWeek().equals(SATURDAY));
-                assertFalse(test.getDayOfWeek().equals(SUNDAY));
+                AssertJUnit.assertTrue(test.isAfter(date));
+                AssertJUnit.assertFalse(test.getDayOfWeek().equals(SATURDAY));
+                AssertJUnit.assertFalse(test.getDayOfWeek().equals(SUNDAY));
 
                 switch (date.getDayOfWeek()) {
                     case FRIDAY:
                     case SATURDAY:
-                        assertEquals(test.getDayOfWeek(), MONDAY);
+                        AssertJUnit.assertEquals(test.getDayOfWeek(), MONDAY);
                         break;
                     default:
-                        assertEquals(date.getDayOfWeek().plus(1), test.getDayOfWeek());
+                        AssertJUnit.assertEquals(date.getDayOfWeek().plus(1), test.getDayOfWeek());
                 }
 
                 if (test.getYear() == 2007) {
                     int dayDiff = test.getDayOfYear() - date.getDayOfYear();
                     switch (date.getDayOfWeek()) {
                         case FRIDAY:
-                            assertEquals(dayDiff, 3);
+                            AssertJUnit.assertEquals(dayDiff, 3);
                             break;
                         case SATURDAY:
-                            assertEquals(dayDiff, 2);
+                            AssertJUnit.assertEquals(dayDiff, 2);
                             break;
                         default:
-                            assertEquals(dayDiff, 1);
+                            AssertJUnit.assertEquals(dayDiff, 1);
                     }
                 } else {
-                    assertEquals(test.getYear(), 2008);
-                    assertEquals(test.getMonth(), JANUARY);
-                    assertEquals(test.getDayOfMonth(), 1);
+                    AssertJUnit.assertEquals(test.getYear(), 2008);
+                    AssertJUnit.assertEquals(test.getMonth(), JANUARY);
+                    AssertJUnit.assertEquals(test.getDayOfMonth(), 1);
                 }
             }
         }
     }
 
-    public void test_nextWorkingDay_yearChange() {
+    @Test public void test_nextWorkingDay_yearChange() {
         LocalDate friday = LocalDate.of(2010, DECEMBER, 31);
         Temporal test = Temporals.nextWorkingDay().adjustInto(friday);
-        assertEquals(LocalDate.of(2011, JANUARY, 3), test);
+        AssertJUnit.assertEquals(LocalDate.of(2011, JANUARY, 3), test);
 
         LocalDate saturday = LocalDate.of(2011, DECEMBER, 31);
         test = Temporals.nextWorkingDay().adjustInto(saturday);
-        assertEquals(LocalDate.of(2012, JANUARY, 2), test);
+        AssertJUnit.assertEquals(LocalDate.of(2012, JANUARY, 2), test);
     }
 
     //-----------------------------------------------------------------------
     // previousWorkingDay()
     //-----------------------------------------------------------------------
-    public void test_previousWorkingDay_serialization() throws IOException, ClassNotFoundException {
+    @Test public void test_previousWorkingDay_serialization() throws IOException, ClassNotFoundException {
         TemporalAdjuster previousWorkingDay = Temporals.previousWorkingDay();
-        assertTrue(previousWorkingDay instanceof Serializable);
+        AssertJUnit.assertTrue(previousWorkingDay instanceof Serializable);
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ObjectOutputStream oos = new ObjectOutputStream(baos);
@@ -139,56 +156,291 @@ public class TestTemporals {
         oos.close();
 
         ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()));
-        assertSame(ois.readObject(), previousWorkingDay);
+        AssertJUnit.assertSame(ois.readObject(), previousWorkingDay);
     }
 
-    public void test_previousWorkingDay() {
+    @Test public void test_previousWorkingDay() {
         for (Month month : Month.values()) {
             for (int i = 1; i <= month.length(false); i++) {
                 LocalDate date = LocalDate.of(2007, month, i);
                 LocalDate test = (LocalDate) Temporals.previousWorkingDay().adjustInto(date);
-                assertTrue(test.isBefore(date));
-                assertFalse(test.getDayOfWeek().equals(SATURDAY));
-                assertFalse(test.getDayOfWeek().equals(SUNDAY));
+                AssertJUnit.assertTrue(test.isBefore(date));
+                AssertJUnit.assertFalse(test.getDayOfWeek().equals(SATURDAY));
+                AssertJUnit.assertFalse(test.getDayOfWeek().equals(SUNDAY));
 
                 switch (date.getDayOfWeek()) {
                     case MONDAY:
                     case SUNDAY:
-                        assertEquals(test.getDayOfWeek(), FRIDAY);
+                        AssertJUnit.assertEquals(test.getDayOfWeek(), FRIDAY);
                         break;
                     default:
-                        assertEquals(date.getDayOfWeek().minus(1), test.getDayOfWeek());
+                        AssertJUnit.assertEquals(date.getDayOfWeek().minus(1), test.getDayOfWeek());
                 }
 
                 if (test.getYear() == 2007) {
                     int dayDiff = test.getDayOfYear() - date.getDayOfYear();
                     switch (date.getDayOfWeek()) {
                         case MONDAY:
-                            assertEquals(dayDiff, -3);
+                            AssertJUnit.assertEquals(dayDiff, -3);
                             break;
                         case SUNDAY:
-                            assertEquals(dayDiff, -2);
+                            AssertJUnit.assertEquals(dayDiff, -2);
                             break;
                         default:
-                            assertEquals(dayDiff, -1);
+                            AssertJUnit.assertEquals(dayDiff, -1);
                     }
                 } else {
-                    assertEquals(test.getYear(), 2006);
-                    assertEquals(test.getMonth(), DECEMBER);
-                    assertEquals(test.getDayOfMonth(), 29);
+                    AssertJUnit.assertEquals(test.getYear(), 2006);
+                    AssertJUnit.assertEquals(test.getMonth(), DECEMBER);
+                    AssertJUnit.assertEquals(test.getDayOfMonth(), 29);
                 }
             }
         }
     }
 
-    public void test_previousWorkingDay_yearChange() {
+    @Test public void test_previousWorkingDay_yearChange() {
         LocalDate monday = LocalDate.of(2011, JANUARY, 3);
         Temporal test = Temporals.previousWorkingDay().adjustInto(monday);
-        assertEquals(LocalDate.of(2010, DECEMBER, 31), test);
+        AssertJUnit.assertEquals(LocalDate.of(2010, DECEMBER, 31), test);
 
         LocalDate sunday = LocalDate.of(2011, JANUARY, 2);
         test = Temporals.previousWorkingDay().adjustInto(sunday);
-        assertEquals(LocalDate.of(2010, DECEMBER, 31), test);
+        AssertJUnit.assertEquals(LocalDate.of(2010, DECEMBER, 31), test);
+    }
+
+    //-------------------------------------------------------------------------
+    @DataProvider(name = "convertAmount")
+    Object[][] data_convertAmount() {
+        return new Object[][] {
+            {2L, NANOS, SECONDS, 0L, 2L},
+            {999_999_999L, NANOS, SECONDS, 0L, 999_999_999L},
+            {1_000_000_000L, NANOS, SECONDS, 1L, 0L},
+            {1_000_000_001L, NANOS, SECONDS, 1L, 1L},
+            
+            {2L, NANOS, MINUTES, 0L, 2L},
+            {59_999_999_999L, NANOS, MINUTES, 0L, 59_999_999_999L},
+            {60_000_000_000L, NANOS, MINUTES, 1L, 0L},
+            {60_000_000_001L, NANOS, MINUTES, 1L, 1L},
+            
+            {2L, NANOS, HOURS, 0L, 2L},
+            {3599_999_999_999L, NANOS, HOURS, 0L, 3599_999_999_999L},
+            {3600_000_000_000L, NANOS, HOURS, 1L, 0L},
+            {3600_000_000_001L, NANOS, HOURS, 1L, 1L},
+            
+            {2L, NANOS, HALF_DAYS, 0L, 2L},
+            {3600_000_000_000L * 12 * 3, NANOS, HALF_DAYS, 3L, 0L},
+            
+            {2L, NANOS, DAYS, 0L, 2L},
+            {3600_000_000_000L * 24 * 3, NANOS, DAYS, 3L, 0L},
+            
+            {2L, NANOS, WEEKS, 0L, 2L},
+            {3600_000_000_000L * 24 * 7 * 3, NANOS, WEEKS, 3L, 0L},
+            
+            {2L, SECONDS, MINUTES, 0L, 2L},
+            {59L, SECONDS, MINUTES, 0L, 59L},
+            {60L, SECONDS, MINUTES, 1L, 0L},
+            {61L, SECONDS, MINUTES, 1L, 1L},
+            
+            {2L, SECONDS, HOURS, 0L, 2L},
+            {3599L, SECONDS, HOURS, 0L, 3599L},
+            {3600L, SECONDS, HOURS, 1L, 0L},
+            {3601L, SECONDS, HOURS, 1L, 1L},
+            
+            {2L, SECONDS, HALF_DAYS, 0L, 2L},
+            {3600L * 12 * 3, SECONDS, HALF_DAYS, 3L, 0L},
+            
+            {2L, SECONDS, DAYS, 0L, 2L},
+            {3600L * 24 * 3, SECONDS, DAYS, 3L, 0L},
+            
+            {2L, SECONDS, WEEKS, 0L, 2L},
+            {3600L * 24 * 7 * 3, SECONDS, WEEKS, 3L, 0L},
+            
+            {2L, MINUTES, HOURS, 0L, 2L},
+            {59L, MINUTES, HOURS, 0L, 59L},
+            {60L, MINUTES, HOURS, 1L, 0L},
+            {61L, MINUTES, HOURS, 1L, 1L},
+            
+            {2L, MINUTES, HALF_DAYS, 0L, 2L},
+            {60L * 12 * 3 + 1, MINUTES, HALF_DAYS, 3L, 1L},
+            
+            {2L, MINUTES, DAYS, 0L, 2L},
+            {60L * 24 * 3 + 1, MINUTES, DAYS, 3L, 1L},
+            
+            {2L, MINUTES, WEEKS, 0L, 2L},
+            {60L * 24 * 7 * 3 + 1, MINUTES, WEEKS, 3L, 1L},
+            
+            {2L, HOURS, HALF_DAYS, 0L, 2L},
+            {12L * 3 + 1, HOURS, HALF_DAYS, 3L, 1L},
+            
+            {2L, HOURS, DAYS, 0L, 2L},
+            {24L * 3 + 1, HOURS, DAYS, 3L, 1L},
+            
+            {2L, HOURS, WEEKS, 0L, 2L},
+            {24L * 7 * 3 + 1, HOURS, WEEKS, 3L, 1L},
+            
+            {1L, HALF_DAYS, DAYS, 0L, 1L},
+            {2L * 3 + 1, HALF_DAYS, DAYS, 3L, 1L},
+            
+            {1L, HALF_DAYS, WEEKS, 0L, 1L},
+            {2L * 7 * 3 + 1, HALF_DAYS, WEEKS, 3L, 1L},
+            
+            {1L, DAYS, WEEKS, 0L, 1L},
+            {7L * 3 + 1, DAYS, WEEKS, 3L, 1L},
+            
+            {2L, SECONDS, NANOS, 2_000_000_000L, 0L},
+            {2L, MINUTES, NANOS, 2_000_000_000L * 60, 0L},
+            {2L, HOURS, NANOS, 2_000_000_000L * 3600, 0L},
+            {2L, HALF_DAYS, NANOS, 2_000_000_000L * 3600 * 12, 0L},
+            {2L, DAYS, NANOS, 2_000_000_000L * 3600 * 24, 0L},
+            {2L, WEEKS, NANOS, 2_000_000_000L * 3600 * 24 * 7, 0L},
+            
+            {2L, MINUTES, SECONDS, 2L * 60, 0L},
+            {2L, HOURS, SECONDS, 2L * 3600, 0L},
+            {2L, HALF_DAYS, SECONDS, 2L * 3600 * 12, 0L},
+            {2L, DAYS, SECONDS, 2L * 3600 * 24, 0L},
+            {2L, WEEKS, SECONDS, 2L * 3600 * 24 * 7, 0L},
+            
+            {2L, HOURS, MINUTES, 2L * 60, 0L},
+            {2L, HALF_DAYS, MINUTES, 2L * 60 * 12, 0L},
+            {2L, DAYS, MINUTES, 2L * 60 * 24, 0L},
+            {2L, WEEKS, MINUTES, 2L * 60 * 24 * 7, 0L},
+            
+            {2L, HALF_DAYS, HOURS, 2L * 12, 0L},
+            {2L, DAYS, HOURS, 2L * 24, 0L},
+            {2L, WEEKS, HOURS, 2L * 24 * 7, 0L},
+            
+            {2L, DAYS, HALF_DAYS, 2L * 2, 0L},
+            {2L, WEEKS, HALF_DAYS, 2L * 2 * 7, 0L},
+            
+            {2L, WEEKS, DAYS, 2L * 7, 0L},
+            
+            {2L * 3 + 1, MONTHS, QUARTER_YEARS, 2L, 1L},
+            {2L * 12 + 1, MONTHS, YEARS, 2L, 1L},
+            {2L * 120 + 1, MONTHS, DECADES, 2L, 1L},
+            {2L * 1200 + 1, MONTHS, CENTURIES, 2L, 1L},
+            {2L * 12000 + 1, MONTHS, MILLENNIA, 2L, 1L},
+            
+            {2L * 4 + 1, QUARTER_YEARS, YEARS, 2L, 1L},
+            {2L * 40 + 1, QUARTER_YEARS, DECADES, 2L, 1L},
+            {2L * 400 + 1, QUARTER_YEARS, CENTURIES, 2L, 1L},
+            {2L * 4000 + 1, QUARTER_YEARS, MILLENNIA, 2L, 1L},
+            
+            {2L * 10 + 1, YEARS, DECADES, 2L, 1L},
+            {2L * 100 + 1, YEARS, CENTURIES, 2L, 1L},
+            {2L * 1000 + 1, YEARS, MILLENNIA, 2L, 1L},
+            
+            {2L * 10 + 1, DECADES, CENTURIES, 2L, 1L},
+            {2L * 100 + 1, DECADES, MILLENNIA, 2L, 1L},
+            
+            {2L * 10 + 1, CENTURIES, MILLENNIA, 2L, 1L},
+            
+            {2L, QUARTER_YEARS, MONTHS, 2L * 3, 0L},
+            {2L, YEARS, MONTHS, 2L * 12, 0L},
+            {2L, DECADES, MONTHS, 2L * 120, 0L},
+            {2L, CENTURIES, MONTHS, 2L * 1200, 0L},
+            {2L, MILLENNIA, MONTHS, 2L * 12000, 0L},
+            
+            {2L, YEARS, QUARTER_YEARS, 2L * 4, 0L},
+            {2L, DECADES, QUARTER_YEARS, 2L * 40, 0L},
+            {2L, CENTURIES, QUARTER_YEARS, 2L * 400, 0L},
+            {2L, MILLENNIA, QUARTER_YEARS, 2L * 4000, 0L},
+            
+            {2L, DECADES, YEARS, 2L * 10, 0L},
+            {2L, CENTURIES, YEARS, 2L * 100, 0L},
+            {2L, MILLENNIA, YEARS, 2L * 1000, 0L},
+            
+            {2L, CENTURIES, DECADES, 2L * 10, 0L},
+            {2L, MILLENNIA, DECADES, 2L * 100, 0L},
+            
+            {2L, MILLENNIA, CENTURIES, 2L * 10, 0L},
+        };
+    }
+
+    @Test(dataProvider = "convertAmount")
+    public void test_convertAmount(
+                    long fromAmount, TemporalUnit fromUnit, TemporalUnit resultUnit,
+                    long resultWhole, long resultRemainder) {
+        long[] result = Temporals.convertAmount(fromAmount, fromUnit, resultUnit);
+        AssertJUnit.assertEquals(result[0], resultWhole);
+        AssertJUnit.assertEquals(result[1], resultRemainder);
+    }
+
+    @Test(dataProvider = "convertAmount")
+    public void test_convertAmount_negative(
+                    long fromAmount, TemporalUnit fromUnit, TemporalUnit resultUnit,
+                    long resultWhole, long resultRemainder) {
+        long[] result = Temporals.convertAmount(-fromAmount, fromUnit, resultUnit);
+        AssertJUnit.assertEquals(result[0], -resultWhole);
+        AssertJUnit.assertEquals(result[1], -resultRemainder);
+    }
+
+    @Test
+    public void test_convertAmountSameUnit_zero() {
+        for (ChronoUnit unit : ChronoUnit.values()) {
+            if (unit != ERAS && unit != FOREVER) {
+                long[] result = Temporals.convertAmount(0, unit, unit);
+                AssertJUnit.assertEquals(result[0], 0);
+                AssertJUnit.assertEquals(result[1], 0);
+            }
+        }
+    }
+
+    @Test
+    public void test_convertAmountSameUnit_nonZero() {
+        for (ChronoUnit unit : ChronoUnit.values()) {
+            if (unit != ERAS && unit != FOREVER) {
+                long[] result = Temporals.convertAmount(2, unit, unit);
+                AssertJUnit.assertEquals(result[0], 2);
+                AssertJUnit.assertEquals(result[1], 0);
+            }
+        }
+    }
+
+    @DataProvider(name = "convertAmountInvalid")
+    Object[][] data_convertAmountInvalid() {
+        return new Object[][] {
+            {SECONDS, MONTHS},
+            {SECONDS, QUARTER_YEARS},
+            {SECONDS, YEARS},
+            {SECONDS, DECADES},
+            {SECONDS, CENTURIES},
+            {SECONDS, MILLENNIA},
+            
+            {MONTHS, SECONDS},
+            {QUARTER_YEARS, SECONDS},
+            {YEARS, SECONDS},
+            {DECADES, SECONDS},
+            {CENTURIES, SECONDS},
+            {MILLENNIA, SECONDS},
+        };
+    }
+
+    @Test(dataProvider = "convertAmountInvalid", expectedExceptions = DateTimeException.class)
+    public void test_convertAmountInvalid(TemporalUnit fromUnit, TemporalUnit resultUnit) {
+        Temporals.convertAmount(1, fromUnit, resultUnit);
+    }
+
+    @DataProvider(name = "convertAmountInvalidUnsupported")
+    Object[][] data_convertAmountInvalidUnsupported() {
+        return new Object[][] {
+            {SECONDS, ERAS},
+            {ERAS, SECONDS},
+            {YEARS, ERAS},
+            {ERAS, YEARS},
+            
+            {SECONDS, FOREVER},
+            {FOREVER, SECONDS},
+            {YEARS, FOREVER},
+            {FOREVER, YEARS},
+            
+            {FOREVER, ERAS},
+            {ERAS, FOREVER},
+        };
+    }
+
+    @Test(dataProvider = "convertAmountInvalidUnsupported", expectedExceptions = UnsupportedTemporalTypeException.class)
+    public void test_convertAmountInvalidUnsupported(TemporalUnit fromUnit, TemporalUnit resultUnit) {
+        Temporals.convertAmount(1, fromUnit, resultUnit);
     }
 
 }
