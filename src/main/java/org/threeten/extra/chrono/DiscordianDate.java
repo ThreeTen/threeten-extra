ec6eaefd31d6da8e42da7cb28e934890c2f2eb1e
Clockwork-Muse <clockwork-muse@outlook.com>
2014-10-08 16:04:10 +0900

    Add DiscordianDate and initial construction tests.

---
+++ src/main/java/org/threeten/extra/chrono/DiscordianDate.java
@@ -0,0 +1,318 @@
+/*
+ * Copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of JSR-310 nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.threeten.extra.chrono;
+
+import static java.time.temporal.ChronoField.EPOCH_DAY;
+
+import java.io.Serializable;
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.chrono.ChronoLocalDate;
+import java.time.chrono.ChronoPeriod;
+import java.time.chrono.Chronology;
+import java.time.temporal.ChronoField;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalQuery;
+import java.time.temporal.TemporalUnit;
+import java.time.temporal.ValueRange;
+
+/**
+ * A date in the Discordian calendar system.
+ * <p>
+ * This date operates using the {@linkplain DiscordianChronology Discordian calendar}.
+ * This calendar system is used by some adherents to Discordianism.
+ * The Discordian differs from the Gregorian in terms of the length of the week and month, and uses an offset year.
+ * Dates are aligned such that {@code 0001-01-01 (Discordian)} is {@code -1165-01-01 (ISO)}.
+ *
+ * <h3>Implementation Requirements</h3>
+ * This class is immutable and thread-safe.
+ * <p>
+ * This class must be treated as a value type. Do not synchronize, rely on the
+ * identity hash code or use the distinction between equals() and ==.
+ */
+public final class DiscordianDate
+        extends AbstractDate
+        implements ChronoLocalDate, Serializable {
+
+    /**
+     * Serialization version.
+     */
+    private static final long serialVersionUID = -4340508226506164852L;
+
+
+    /**
+     * The proleptic year.
+     */
+    private final int prolepticYear;
+    /**
+     * The month.
+     */
+    private final short month;
+    /**
+     * The day.
+     */
+    private final short day;
+
+    //-----------------------------------------------------------------------
+    /**
+     * Obtains the current {@code DiscordianDate} from the system clock in the default time-zone.
+     * <p>
+     * This will query the {@link Clock#systemDefaultZone() system clock} in the default
+     * time-zone to obtain the current date.
+     * <p>
+     * Using this method will prevent the ability to use an alternate clock for testing
+     * because the clock is hard-coded.
+     *
+     * @return the current date using the system clock and default time-zone, not null
+     */
+    public static DiscordianDate now() {
+        return now(Clock.systemDefaultZone());
+    }
+
+    /**
+     * Obtains the current {@code DiscordianDate} from the system clock in the specified time-zone.
+     * <p>
+     * This will query the {@link Clock#system(ZoneId) system clock} to obtain the current date.
+     * Specifying the time-zone avoids dependence on the default time-zone.
+     * <p>
+     * Using this method will prevent the ability to use an alternate clock for testing
+     * because the clock is hard-coded.
+     *
+     * @param zone  the zone ID to use, not null
+     * @return the current date using the system clock, not null
+     */
+    public static DiscordianDate now(ZoneId zone) {
+        return now(Clock.system(zone));
+    }
+
+    /**
+     * Obtains the current {@code DiscordianDate} from the specified clock.
+     * <p>
+     * This will query the specified clock to obtain the current date - today.
+     * Using this method allows the use of an alternate clock for testing.
+     * The alternate clock may be introduced using {@linkplain Clock dependency injection}.
+     *
+     * @param clock  the clock to use, not null
+     * @return the current date, not null
+     * @throws DateTimeException if the current date cannot be obtained
+     */
+    public static DiscordianDate now(Clock clock) {
+        LocalDate now = LocalDate.now(clock);
+        return DiscordianDate.ofEpochDay(now.toEpochDay());
+    }
+
+    /**
+     * Obtains a {@code DiscordianDate} representing a date in the Discordian calendar
+     * system from the proleptic-year, month-of-year and day-of-month fields.
+     * <p>
+     * This returns a {@code DiscordianDate} with the specified fields.
+     * The day must be valid for the year and month, otherwise an exception will be thrown.
+     *
+     * @param prolepticYear  the Discordian proleptic-year
+     * @param month  the Discordian month-of-year, from 1 to 5
+     * @param dayOfMonth  the Discordian day-of-month, from 1 to 73
+     * @return the date in Discordian calendar system, not null
+     * @throws DateTimeException if the value of any field is out of range,
+     *  or if the day-of-month is invalid for the month-year
+     */
+    public static DiscordianDate of(int prolepticYear, int month, int dayOfMonth) {
+        return DiscordianDate.create(prolepticYear, month, dayOfMonth);
+    }
+
+    /**
+     * Obtains a {@code DiscordianDate} from a temporal object.
+     * <p>
+     * This obtains a date in the Discordian calendar system based on the specified temporal.
+     * A {@code TemporalAccessor} represents an arbitrary set of date and time information,
+     * which this factory converts to an instance of {@code DiscordianDate}.
+     * <p>
+     * The conversion typically uses the {@link ChronoField#EPOCH_DAY EPOCH_DAY}
+     * field, which is standardized across calendar systems.
+     * <p>
+     * This method matches the signature of the functional interface {@link TemporalQuery}
+     * allowing it to be used as a query via method reference, {@code DiscordianDate::from}.
+     *
+     * @param temporal  the temporal object to convert, not null
+     * @return the date in Discordian calendar system, not null
+     * @throws DateTimeException if unable to convert to a {@code DiscordianDate}
+     */
+    public static DiscordianDate from(TemporalAccessor temporal) {
+        if (temporal instanceof DiscordianDate) {
+            return (DiscordianDate) temporal;
+        }
+        return DiscordianDate.ofEpochDay(temporal.getLong(EPOCH_DAY));
+    }
+
+    //-----------------------------------------------------------------------
+    /**
+     * Obtains a {@code DiscordianDate} representing a date in the Discordian calendar
+     * system from the proleptic-year and day-of-year fields.
+     * <p>
+     * This returns a {@code DiscordianDate} with the specified fields.
+     * The day must be valid for the year, otherwise an exception will be thrown.
+     *
+     * @param prolepticYear  the Discordian proleptic-year
+     * @param dayOfYear  the Discordian day-of-year, from 1 to 366
+     * @return the date in Discordian calendar system, not null
+     * @throws DateTimeException if the value of any field is out of range,
+     *  or if the day-of-year is invalid for the year
+     */
+    static DiscordianDate ofYearDay(int prolepticYear, int dayOfYear) {
+     // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * Obtains a {@code DiscordianDate} representing a date in the Discordian calendar
+     * system from the epoch-day.
+     *
+     * @param epochDay  the epoch day to convert based on 1970-01-01 (ISO)
+     * @return the date in Discordian calendar system, not null
+     * @throws DateTimeException if the epoch-day is out of range
+     */
+    static DiscordianDate ofEpochDay(final long epochDay) {
+     // TODO Auto-generated method stub
+        return null;
+    }
+
+    private static DiscordianDate resolvePreviousValid(int prolepticYear, int month, int day) {
+     // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * Creates a {@code DiscordianDate} validating the input.
+     *
+     * @param prolepticYear  the Discordian proleptic-year
+     * @param month  the Discordian month-of-year, from 1 to 5
+     * @param dayOfMonth  the Discordian day-of-month, from 1 to 73
+     * @return the date in Discordian calendar system, not null
+     * @throws DateTimeException if the value of any field is out of range,
+     *  or if the day-of-year is invalid for the month-year
+     */
+    static DiscordianDate create(int prolepticYear, int month, int dayOfMonth) {
+     // TODO Auto-generated method stub
+        return null;
+    }
+
+    //-----------------------------------------------------------------------
+    /**
+     * Creates an instance from validated data.
+     *
+     * @param prolepticYear  the Discordian proleptic-year
+     * @param month  the Discordian month, from 1 to 5
+     * @param dayOfMonth  the Discordian day-of-month, from 1 to 73
+     */
+    private DiscordianDate(int prolepticYear, int month, int dayOfMonth) {
+        this.prolepticYear = prolepticYear;
+        this.month = (short) month;
+        this.day = (short) dayOfMonth;
+    }
+
+    /**
+     * Validates the object.
+     *
+     * @return the resolved date, not null
+     */
+    private Object readResolve() {
+        return DiscordianDate.create(prolepticYear, month, day);
+    }
+
+    @Override
+    int getProlepticYear() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    int getMonth() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    int getDayOfMonth() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    int getDayOfYear() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    AbstractDate withDayOfYear(int value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    ValueRange rangeAlignedWeekOfMonth() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    AbstractDate resolvePrevious(int newYear, int newMonth, int dayOfMonth) {
+        return resolvePreviousValid(newYear, newMonth, dayOfMonth);
+    }
+
+    @Override
+    public Chronology getChronology() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int lengthOfMonth() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public long until(Temporal arg0, TemporalUnit arg1) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public ChronoPeriod until(ChronoLocalDate arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
--- src/test/java/org/threeten/extra/chrono/TestDiscordianChronology.java
+++ src/test/java/org/threeten/extra/chrono/TestDiscordianChronology.java
@@ -31,9 +31,16 @@
  */
 package org.threeten.extra.chrono;
 
+import static java.time.temporal.ChronoUnit.DAYS;
+import static org.testng.Assert.assertEquals;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.Period;
 import java.time.chrono.Chronology;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -61,6 +68,147 @@ public class TestDiscordianChronology {
         Assert.assertEquals(chrono, DiscordianChronology.INSTANCE);
         Assert.assertEquals(chrono.getId(), "Discordian");
         Assert.assertEquals(chrono.getCalendarType(), "discordian");
+    }
+
+    // -----------------------------------------------------------------------
+    // creation, toLocalDate()
+    // -----------------------------------------------------------------------
+    @DataProvider(name="samples")
+    Object[][] data_samples() {
+        return new Object[][] {
+            {DiscordianDate.of(1167, 1, 1), LocalDate.of(1, 1, 1)},
+            {DiscordianDate.of(1167, 1, 2), LocalDate.of(1, 1, 2)},
+            {DiscordianDate.of(1167, 1, 3), LocalDate.of(1, 1, 3)},
+
+            {DiscordianDate.of(1167, 1, 57), LocalDate.of(1, 2, 26)},
+            {DiscordianDate.of(1167, 1, 58), LocalDate.of(1, 2, 27)},
+            {DiscordianDate.of(1167, 1, 59), LocalDate.of(1, 2, 28)},
+            {DiscordianDate.of(1167, 1, 60), LocalDate.of(1, 3, 1)},
+
+            {DiscordianDate.of(1170, 1, 57), LocalDate.of(4, 2, 26)},
+            {DiscordianDate.of(1170, 1, 58), LocalDate.of(4, 2, 27)},
+            {DiscordianDate.of(1170, 1, 59), LocalDate.of(4, 2, 28)},
+            {DiscordianDate.of(1170, 0, 0), LocalDate.of(4, 2, 29)},
+            {DiscordianDate.of(1170, 1, 60), LocalDate.of(4, 3, 1)},
+
+            {DiscordianDate.of(1266, 1, 57), LocalDate.of(100, 2, 26)},
+            {DiscordianDate.of(1266, 1, 58), LocalDate.of(100, 2, 27)},
+            {DiscordianDate.of(1266, 1, 59), LocalDate.of(100, 2, 28)},
+            {DiscordianDate.of(1266, 1, 60), LocalDate.of(100, 3, 1)},
+            {DiscordianDate.of(1266, 1, 61), LocalDate.of(100, 3, 2)},
+
+            {DiscordianDate.of(1166, 5, 73), LocalDate.of(0, 12, 31)},
+            {DiscordianDate.of(1166, 5, 72), LocalDate.of(0, 12, 30)},
+
+            {DiscordianDate.of(2748, 4, 68), LocalDate.of(1582, 10, 14)},
+            {DiscordianDate.of(2748, 4, 69), LocalDate.of(1582, 10, 15)},
+            {DiscordianDate.of(3111, 5, 24), LocalDate.of(1945, 11, 12)},
+
+            {DiscordianDate.of(3178, 3, 40), LocalDate.of(2012, 7, 5)},
+            {DiscordianDate.of(3178, 3, 41), LocalDate.of(2012, 7, 6)},
+        };
+    }
+
+    @Test(dataProvider="samples")
+    public void test_LocalDate_from_DiscordianDate(DiscordianDate discordian, LocalDate iso) {
+        assertEquals(LocalDate.from(discordian), iso);
+    }
+
+    @Test(dataProvider="samples")
+    public void test_DiscordianDate_from_LocalDate(DiscordianDate discordian, LocalDate iso) {
+        assertEquals(DiscordianDate.from(iso), discordian);
+    }
+
+    @Test(dataProvider="samples")
+    public void test_DiscordianDate_chronology_dateEpochDay(DiscordianDate discordian, LocalDate iso) {
+        assertEquals(DiscordianChronology.INSTANCE.dateEpochDay(iso.toEpochDay()), discordian);
+    }
+
+    @Test(dataProvider="samples")
+    public void test_DiscordianDate_toEpochDay(DiscordianDate discordian, LocalDate iso) {
+        assertEquals(discordian.toEpochDay(), iso.toEpochDay());
+    }
+
+    @Test(dataProvider="samples")
+    public void test_DiscordianDate_until_DiscordianDate(DiscordianDate discordian, LocalDate iso) {
+        assertEquals(discordian.until(discordian), DiscordianChronology.INSTANCE.period(0, 0, 0));
+    }
+
+    @Test(dataProvider="samples")
+    public void test_DiscordianDate_until_LocalDate(DiscordianDate discordian, LocalDate iso) {
+        assertEquals(discordian.until(iso), DiscordianChronology.INSTANCE.period(0, 0, 0));
+    }
+
+    @Test(dataProvider="samples")
+    public void test_LocalDate_until_DiscordianDate(DiscordianDate discordian, LocalDate iso) {
+        assertEquals(iso.until(discordian), Period.ZERO);
+    }
+
+    @Test(dataProvider="samples")
+    public void test_Chronology_date_Temporal(DiscordianDate discordian, LocalDate iso) {
+        assertEquals(DiscordianChronology.INSTANCE.date(iso), discordian);
+    }
+
+    @Test(dataProvider="samples")
+    public void test_plusDays(DiscordianDate discordian, LocalDate iso) {
+        assertEquals(LocalDate.from(discordian.plus(0, DAYS)), iso);
+        assertEquals(LocalDate.from(discordian.plus(1, DAYS)), iso.plusDays(1));
+        assertEquals(LocalDate.from(discordian.plus(35, DAYS)), iso.plusDays(35));
+        assertEquals(LocalDate.from(discordian.plus(-1, DAYS)), iso.plusDays(-1));
+        assertEquals(LocalDate.from(discordian.plus(-60, DAYS)), iso.plusDays(-60));
+    }
+
+    @Test(dataProvider="samples")
+    public void test_minusDays(DiscordianDate discordian, LocalDate iso) {
+        assertEquals(LocalDate.from(discordian.minus(0, DAYS)), iso);
+        assertEquals(LocalDate.from(discordian.minus(1, DAYS)), iso.minusDays(1));
+        assertEquals(LocalDate.from(discordian.minus(35, DAYS)), iso.minusDays(35));
+        assertEquals(LocalDate.from(discordian.minus(-1, DAYS)), iso.minusDays(-1));
+        assertEquals(LocalDate.from(discordian.minus(-60, DAYS)), iso.minusDays(-60));
+    }
+
+    @Test(dataProvider="samples")
+    public void test_until_DAYS(DiscordianDate discordian, LocalDate iso) {
+        assertEquals(discordian.until(iso.plusDays(0), DAYS), 0);
+        assertEquals(discordian.until(iso.plusDays(1), DAYS), 1);
+        assertEquals(discordian.until(iso.plusDays(35), DAYS), 35);
+        assertEquals(discordian.until(iso.minusDays(40), DAYS), -40);
+    }
+
+    @DataProvider(name="badDates")
+    Object[][] data_badDates() {
+        return new Object[][] {
+            {1900, 0, 0},
+
+            {1900, -1, 1},
+            {1900, 0, 1},
+            {1900, 6, 1},
+            {1900, 7, 1},
+
+            {1900, 1, -1},
+            {1900, 1, 0},
+            {1900, 1, 74},
+
+            {1900, 0, 0},
+
+            {1900, 5, -1},
+            {1900, 5, 0},
+            {1900, 5, 74},
+
+            {1900, 2, 74},
+            {1900, 3, 74},
+            {1900, 4, 74},
+        };
+    }
+
+    @Test(dataProvider="badDates", expectedExceptions=DateTimeException.class)
+    public void test_badDates(int year, int month, int dom) {
+        DiscordianDate.of(year, month, dom);
+    }
+
+    @Test(expectedExceptions=DateTimeException.class)
+    public void test_chronology_dateYearDay_badDate() {
+        DiscordianChronology.INSTANCE.dateYearDay(2001, 366);
     }
 
 }
