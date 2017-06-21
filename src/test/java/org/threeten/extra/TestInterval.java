02d74b252c0c86713b4877449e3c141d9c1c05b4
Carlo Dapor <catull@gmail.com>
2017-06-21 23:52:04 +0200

    Interval#parse accepts 4 different formats, which should be reflected in the documentation.  Also, added an minimal / maximal bounds check.

--- src/test/java/org/threeten/extra/TestInterval.java
+++ src/test/java/org/threeten/extra/TestInterval.java
@@ -153,6 +153,10 @@ public class TestInterval {
         Interval.of(NOW1, (Duration) null);
     }
 
+    /* Lower and upper bound for Intervals */
+    private static final Instant MIN_OFFSET_DATE_TIME = OffsetDateTime.MIN.plusDays(1L).toInstant();
+    private static final Instant MAX_OFFSET_DATE_TIME = OffsetDateTime.MAX.minusDays(1L).toInstant();
+
     //-----------------------------------------------------------------------
     @DataProvider(name = "parseValid")
     Object[][] data_parseValid() {
@@ -167,6 +171,7 @@ public class TestInterval {
             {NOW1.atOffset(ZoneOffset.ofHours(2)) + "/" + NOW2.atOffset(ZoneOffset.ofHours(2)), NOW1, NOW2},
             {NOW1.atOffset(ZoneOffset.ofHours(2)) + "/" + NOW2.atOffset(ZoneOffset.ofHours(3)), NOW1, NOW2},
             {NOW1.atOffset(ZoneOffset.ofHours(2)) + "/" + NOW2.atOffset(ZoneOffset.ofHours(2)).toLocalDateTime(), NOW1, NOW2},
+            {MIN_OFFSET_DATE_TIME.toString() + '/' + MAX_OFFSET_DATE_TIME, MIN_OFFSET_DATE_TIME, MAX_OFFSET_DATE_TIME}
         };
     }
     
