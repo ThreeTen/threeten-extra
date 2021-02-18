99f001320fc010e1457f1e8d2189b28b27072a60
Stephen Colebourne <scolebourne@joda.org>
2021-02-18 10:18:12 +0000

    Clarify Javadoc of YearWeek.of(Year, int)

--- src/main/java/org/threeten/extra/YearWeek.java
+++ src/main/java/org/threeten/extra/YearWeek.java
@@ -175,18 +175,25 @@ public final class YearWeek
 
     //-----------------------------------------------------------------------
     /**
-     * Obtains an instance of {@code YearWeek} from a week-based-year and week.
+     * Obtains an instance of {@code YearWeek} from a year and week.
      * <p>
      * If the week is 53 and the year does not have 53 weeks, week one of the following
      * year is selected.
+     * <p>
+     * Note that this class is based on the week-based-year which aligns to Monday to Sunday weeks,
+     * whereas {@code Year} is intended to represent standard years aligned from January to December.
+     * This difference may be seen at the start and/or end of the year.
+     * This method treats the standard year as though it is the week-based-year.
+     * Thus, {@code YearWeek.of(Year.of(2020), 1)} creates an object where Monday and Tuesday of the week
+     * are actually the last two days of 2019.
      *
-     * @param year  the week-based-year to represent, not null
+     * @param year  the year to represent, not null
      * @param week  the week-of-week-based-year to represent, from 1 to 53
      * @return the year-week, not null
      * @throws DateTimeException if the week value is invalid
      */
     public static YearWeek of(Year year, int week) {
-      return of(year.getValue(), week);
+        return of(year.getValue(), week);
     }
 
     /**
